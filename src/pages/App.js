7b5a6197349212a1daa872c4c6ef68196ed5bf36
Camila Zucchi <camilazucchi@outlook.com>
2022-11-16 14:25:14 -0300

    creating api with axios

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { DiGithubBadge } from "react-icons/di";
 import { IconContext } from "react-icons";
 import { Container } from "./styles";
@@ -5,6 +6,12 @@ import Input from "../components/Input";
 import ItemRepo from "../components/ItemRepo";
 
 export default function App() {
+
+  const [repos, setRepos] = useState([]);
+
+
+  
+
   return (
     <Container>
       <IconContext.Provider value={{ color: "#fdfffc", size: "5rem" }}>
