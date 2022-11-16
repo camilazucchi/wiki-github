b06379d5327af50fb21d19d5ae2854a662917fb9
Camila Zucchi <camilazucchi@outlook.com>
2022-11-16 13:21:37 -0300

    initial commit

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import { DiGithubBadge } from "react-icons/di";
+import { Container } from "./styles";
+
+function App() {
+  return (
+    <Container>
+    <DiGithubBadge />
+    teste
+    </Container>
+  )
+}
+
+export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        border-sizing: border-box;
+    }
+
+    body {
+        background-color: #2f3e46;
+        color: #cad2c5;
+        font-family: 'Montserrat', sans-serif;
+    }
+`
